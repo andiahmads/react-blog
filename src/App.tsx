54f72466b7f20fb474b3261ece687eb522f839dc
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2021-12-22 15:09:33 +0700

    layout config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PagesRender from './PagesRender'
+import Header from './components/global/Header'
+import Footer from './components/global/Footer'
 
-const App=() => {
+const App = () => {
   return (
     <div className="container">
       <Router>
+        <Header />
         <Switch>
-          <Route exact path="/" component={PagesRender}/>
-          <Route exact path="/:page" component={PagesRender}/>
-          <Route exact path="/:page/:slug" component={PagesRender}/>
+          <Route exact path="/" component={PagesRender} />
+          <Route exact path="/:page" component={PagesRender} />
+          <Route exact path="/:page/:slug" component={PagesRender} />
         </Switch>
+        <Footer />
 
       </Router>
     </div>
---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="text-center bg-light py-4">
+            <h6>Santara, All rights reserved.</h6>
+            <a href="https://github.com/andiahmads" target="_blank" 
+            className="mb-2 d-block text-info"
+            rel="noopener">Github.com</a>
+            <p>Copyright © 2021</p>   
+        </div>
+    )
+}
+
+export default Footer
